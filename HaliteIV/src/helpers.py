790e105fa1b85823000784277a11714f3e673a79
russell tian <russell.tian1996@gmail.com>
2020-06-21 14:38:33 -0400

    get source

---
+++ HaliteIV/src/helpers.py
@@ -0,0 +1,40 @@
+import random
+
+'''
+The following helper functions are provided by kaggle
+'''
+# Returns the position in some direction relative to the current position (pos)
+def get_col_row(size, pos):
+    return (pos % size, pos // size)
+
+def get_to_pos(size, pos, direction):
+    col, row = get_col_row(size, pos)
+    if direction == "NORTH":
+        return pos - size if pos >= size else size ** 2 - size + col
+    elif direction == "SOUTH":
+        return col if pos + size >= size ** 2 else pos + size
+    elif direction == "EAST":
+        return pos + 1 if col < size - 1 else row * size
+    elif direction == "WEST":
+        return pos - 1 if col > 0 else (row + 1) * size - 1
+
+# Especially useful for figuring out how much halite is around you
+def getAdjacent(pos, size):
+    return [
+        get_to_pos(size, pos, "NORTH"),
+        get_to_pos(size, pos, "SOUTH"),
+        get_to_pos(size, pos, "WEST"),
+        get_to_pos(size, pos, "EAST"),
+    ]
+
+
+# Returns best direction to move from one position (fromPos) to another (toPos)
+# Example: If I'm at pos 0 and want to get to pos 55, which direction should I choose?
+def getDirTo(fromPos, toPos, size):
+    fromY, fromX = divmod(fromPos, size)
+    toY, toX = divmod(toPos, size)
+    if fromY < toY: return "SOUTH"
+    if fromY > toY: return "NORTH"
+    if fromX < toX: return "EAST"
+    if fromX > toX: return "WEST"
+
--- HaliteIV/src/main.py
+++ HaliteIV/src/main.py
@@ -1,4 +1,7 @@
 from kaggle_environments import evaluate, make
-env = make("halite", configuration={ "episodeSteps": 50 }, debug=True)
+env = make("halite", configuration={ "episodeSteps": 100 }, debug=True)
 env.run(["agent.py", "random","random","random"])
-env.render(mode="html", width=800, height=600)
+out = env.render(mode="html", width=500, height=450)
+f = open("replay.html", "w")
+f.write(out)
+f.close()
